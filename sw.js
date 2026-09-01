9ca42a555769d799c726f8ea44f405b0f27964fb
Marine-Data <m.zinzen04@gmail.com>
2026-09-01 09:07:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -35,8 +35,8 @@
 //       lun 24 déjeuner au Colombier + courses 17h + dîner maison ; mar 25 bateau à 8 ;
 //       mer 26 plongée à 5 + boulangerie + dîner maison ; jeu 27 dîner maison (plus Colombier) ;
 //       ven 28 journée à définir ; sam 29 Petit Sud au Mourillon. Corvées repas recalculées.
-// v43 = carnet de voyage HTML (carnet.html) ajouté + accès depuis l'accueil (Pour toi aujourd'hui).
-const CACHE_VERSION = 'saraillon-v43';
+// v48 = carnet : section Défis // v47 = carnet : vidéo de la mission de Marine (compliment à Nawaëlle) intégrée. quêtes + vidéo du défi de Chunfei.
+const CACHE_VERSION = 'saraillon-v48';
 const APP_SHELL = [
   './',
   './index.html',
